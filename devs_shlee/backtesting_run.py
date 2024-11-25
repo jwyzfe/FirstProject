2922861639a0d095c1b787bb2be161c50b02176e
Huey <demonic0319@gmail.com>
2024-11-25 02:54:24 +0000

    feat: backtestiing update

---
+++ devs_shlee/backtesting_run.py
@@ -0,0 +1,218 @@
+
+# 스크래퍼 import 
+from apscheduler.schedulers.background import BackgroundScheduler
+# mongo DB 동작
+from pymongo import MongoClient
+# time.sleep
+import time
+# 
+import pandas as pd
+
+# 직접 만든 class나 func을 참조하려면 꼭 필요 => main processor가 경로를 잘 몰라서 알려주어야함.
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+                
+from commons.config_reader import read_config # config read 용       
+from commons.mongo_insert_recode import connect_mongo as connect_mongo_insert
+from commons.api_send_requester import ApiRequester
+from commons.templates.sel_iframe_courtauction import iframe_test
+from commons.templates.bs4_do_scrapping import bs4_scrapping
+from commons.mongo_find_recode import connect_mongo as connect_mongo_find
+
+# 직접 구현한 부분을 import 해서 scheduler에 등록
+from devs_shlee.api_stockprice_yfinance_daily import api_stockprice_yfinance 
+from devs_shlee.sel_comment_scrap_stocktwits import comment_scrap_stocktwits 
+
+# common 에 넣을 예정
+def register_job_with_mongo(client, ip_add, db_name, col_name_work, col_name_dest, func, insert_data):
+
+    try:
+        if client is None:
+            client = MongoClient(ip_add)
+        symbols = connect_mongo_find.get_unfinished_ready_records(client, db_name, col_name_work)
+          
+        # symbols가 비어있는지 확인
+        if symbols.empty:
+            print("zero recode. skip schedule")
+            return
+        
+        # 60개씩 제한 # 40 
+        BATCH_SIZE = 20
+        symbols_batch = symbols.head(BATCH_SIZE)  # 처음 60개만 선택
+        
+        # symbol 컬럼만 리스트로 변환 => 추후 더 조치 필요 
+        symbol_list = symbols_batch[insert_data].tolist()
+
+        # 선택된 symbol 처리
+        result_data = func(symbol_list)
+        if client is None:
+            client = MongoClient(ip_add)
+        result_list = connect_mongo_insert.insert_recode_in_mongo(client, db_name, col_name_dest, result_data)
+    
+        # 처리된 60개의 symbol에 대해서만 상태 업데이트
+        update_data_list = []
+        for _, row in symbols_batch.iterrows():
+            update_data = {
+                'ref_id': row['_id'],  # 원본 레코드의 ID를 참조 ID로 저장
+                'iswork': 'fin',
+                insert_data : row[insert_data]
+            }
+            update_data_list.append(update_data)
+
+        if client is None:
+            client = MongoClient(ip_add)
+        result_list = connect_mongo_insert.insert_recode_in_mongo(client, db_name, col_name_work, update_data_list)
+
+    except Exception as e:
+        print(e)
+        # client.close()
+
+    return 
+
+# common 에 넣을 예정
+def register_job_with_mongo_cron(client, ip_add, db_name, col_name_work, col_name_dest, func, insert_data):
+    try:
+        if client is None:
+            client = MongoClient(ip_add)
+
+        # col_name_work가 None인지 확인
+        if col_name_work == '':
+            # col_name_work가 None인 경우
+            result_data = func()  # func() 호출
+            print("fin : func")
+            result_list = connect_mongo_insert.insert_recode_in_mongo(client, db_name, col_name_dest, result_data)
+            print("fin : insert data ")
+        else:
+            # col_name_work가 None이 아닌 경우
+            symbols = connect_mongo_find.get_unfinished_ready_records(client, db_name, col_name_work)
+            
+            # symbols가 비어있는지 확인
+            if symbols.empty:
+                print("zero record. skip schedule")
+                return
+            
+            # 20개씩 제한
+            BATCH_SIZE = 7
+            symbols_batch = symbols.head(BATCH_SIZE)  # 처음 20개만 선택
+            
+            # symbol 컬럼만 리스트로 변환
+            symbol_list = symbols_batch[insert_data].tolist()
+
+            # 선택된 symbol 처리
+            result_data = func(symbol_list)
+            print("fin : func")
+            result_list = connect_mongo_insert.insert_recode_in_mongo(client, db_name, col_name_dest, result_data)
+            print("fin : insert data ")
+        
+            # 처리된 20개의 symbol에 대해서만 상태 업데이트
+            update_data_list = []
+            for _, row in symbols_batch.iterrows():
+                update_data = {
+                    'ref_id': row['_id'],  # 원본 레코드의 ID를 참조 ID로 저장
+                    'iswork': 'fin',
+                    insert_data: row[insert_data]
+                }
+                update_data_list.append(update_data)
+
+            result_list = connect_mongo_insert.insert_recode_in_mongo(client, db_name, col_name_work, update_data_list)
+
+    except Exception as e:
+        print(e)
+        # client.close()  # 필요 시 클라이언트 닫기
+
+    return
+
+
+def run():
+
+    config = read_config()
+    ip_add = config['MongoDB_remote']['ip_add']
+    db_name = config['MongoDB_remote']['db_name']
+
+    # MongoDB 서버에 연결 
+    client = MongoClient(ip_add) 
+
+    scheduler = BackgroundScheduler()
+  
+
+    # 여기에 함수 등록 
+    # 넘길 변수 없으면 # insert_data = []
+    insert_data = "symbol" # [val1,val2,val3]
+    
+    '''
+    func : 수행할 일 함수
+    args : 그 일에 필요한 파라미터 => work 디비에 등록하면, 해당 컬럼만 리스트로 전달
+    target : 최종 저장할 데이터 베이스 컬렉션 이름 
+    work : 일 시킬 내용 들어있는 데이터 베이스 컬렉션 이름
+
+    '''
+    # 스케줄 설정을 위한 딕셔너리
+    SCHEDULE_CONFIGS = {
+        'news_5h': {
+            'trigger': 'interval',
+            'hours': 5,
+        },
+        'comment_1h': {
+            'trigger': 'interval',
+            'hours': 1,
+        },
+        'comment_30m': {
+            'trigger': 'interval',
+            'minutes': 30,
+        }
+    }
+
+    func_list = [
+        { 
+            "func": api_stockprice_yfinance.get_stockprice_yfinance_daily, 
+            "args": "symbol", 
+            "target": 'COL_STOCKPRICE_DAILY', 
+            "work": "COL_STOCKPRICE_DAILY_WORK",
+            "schedule": "comment_30m"
+        }
+    ]
+
+    for func in func_list:
+        schedule_config = SCHEDULE_CONFIGS[func['schedule']]
+        
+        scheduler.add_job(
+            register_job_with_mongo_cron,                         
+            trigger=schedule_config['trigger'],
+            coalesce=True, 
+            max_instances=1,
+            id=func['func'].__name__,
+            args=[client, ip_add, db_name, func['work'], func['target'], func['func'], func['args']],
+            **{k: v for k, v in schedule_config.items() if k != 'trigger'}
+        )
+        '''
+        scheduler.add_job(
+                    register_job_with_mongo_cron,                         
+                    trigger='cron',  # 크론 트리거 사용
+                    second='0',      # 매 분의 0초에 실행
+                    minute='*',      # 매 분마다 실행
+                    hour='*',        # 매 시간마다 실행
+                    day='*',         # 매일 실행
+                    month='*',       # 매월 실행
+                    day_of_week='*', # 매주 실행
+                    coalesce=True, 
+                    max_instances=1,
+                    id=func['func'].__name__,  # 독립적인 함수 이름 주어야 함.
+                    args=[client, ip_add, db_name, func['work'], func['target'], func['func'], func['args']]
+                )
+        
+        '''
+    
+    
+    scheduler.start()
+
+    while True:
+
+        pass
+    
+    return True
+
+
+if __name__ == '__main__':
+    run()
+    pass
--- devs_shlee/test_yfinance.py
+++ devs_shlee/test_yfinance.py
@@ -15,174 +15,12 @@ import yfinance as yf
 # padas
 import pandas as pd
 # datetime
-from datetime import datetime 
+from datetime import datetime, timedelta 
+
 import pytz
 
-class api_stockprice_yfinance:
 
-    def get_stockprice_yfinance(symbol_list):
-        return_histlist = pd.DataFrame()  # 빈 DataFrame 생성
-        print(f"start:{datetime.now(pytz.timezone('Asia/Seoul'))}")
-        for symbol in symbol_list:
-            msft = yf.Ticker(symbol) # "MSFT"
-            # get all stock info
-            msft.info
-            # get historical market data
-            hist = msft.history(period="max")
-            if hist is not None and not hist.empty:  # hist가 None이 아니고 비어있지 않은 경우
-                hist['symbol'] = symbol  # 'symbol' 컬럼 추가
-                # 인덱스를 'Date' 컬럼으로 변환
-                df_with_date = hist.reset_index()
-                # DataFrame을 레코드 리스트로 변환 후 머지
-                return_histlist = pd.concat([return_histlist, df_with_date])
-            print(f"loop:{datetime.now(pytz.timezone('Asia/Seoul'))}")
-        # print(hist)
-
-        return return_histlist
-    
-
-'''
- "095570",
-    "006840",
-    "282330",
-    "027410",
-    "138930",
-    "001460",
-    "001040",
-    "011150",
-    "000590",
-    "012030",
-    "016610",
-    "005830",
-    "000990",
-    "139130",
-    "001530",
-    "000210",
-    "375500",
-    "155660",
-    "069730",
-    "017860",
-    "017940",
-    "365550",
-    "383220",
-    "007700",
-    "006360",
-    "078930",
-    "012630",
-    "294870",
-    "097230",
-    "014790",
-    "204320",
-    "060980",
-    "035000",
-    "003560",
-    "175330",
-    "234080",
-    "001060",
-    "096760",
-    "105560",
-    "432320",
-    "009070",
-    "003620",
-    "016380",
-    "001390",
-    "033180",
-    "001940",
-    "025000",
-    "092230",
-    "000040",
-    "093050",
-    "034220",
-    "003550",
-    "051900",
-    "373220",
-    "032640",
-    "011070",
-    "066570",
-    "051910",
-    "079550",
-    "010120",
-    "000680",
-    "006260",
-    "229640",
-    "108320",
-    "001120",
-    "023150",
-    "035420",
-    "181710",
-    "338100",
-    "034310",
-    "008260",
-    "004250",
-    "456040",
-    "010950",
-    "005090",
-    "001380",
-    "001770",
-    "002360",
-    "009160",
-    "123700",
-    "025530",
-    "011790",
-    "018670",
-    "001740",
-    "210980",
-    "395400",
-    "034730",
-    "402340",
-    "361610",
-    "100090",
-    "096770",
-    "001510",
-    "285130",
-    "017670",
-    "003570",
-    "064960",
-    "100840",
-    "036530",
-    "005610",
-    "465770",
-    "011810",
-    "077970",
-    "084870",
-    "002710",
-    "024070",
-    "000500",
-    "000860",
-    "035250",
-    "011420",
-    "002100",
-    "009450",
-    "267290",
-    "012320",
-    "000050",
-    "214390",
-    "012610",
-    "009140",
-    "013580",
-    "012200",
-    "002140",
-    "010130",
-    "002240",
-    "009290",
-    "017040",
-    "017900",
-    "037710",
-    "030610",
-    "339770",
-    "007690",
-    "001140",
-    "002720",
-    "114090",
-    "083420",
-    "014530",
-    "014280",
-    "008870",
-    "001570",
-
-'''
-
-def download_minute_data(ticker_symbol, start_date, end_date):
+def history_minute_data(ticker_symbol, start_date, end_date):
     """
     1분 간격의 데이터를 다운로드하는 함수입니다.
     
@@ -212,6 +50,30 @@ def download_minute_data(ticker_symbol, start_date, end_date):
     
     return data
 
+
+def download_minute_data(ticker_symbol, start_date, end_date):
+    """
+    1분 간격의 데이터를 다운로드하는 함수입니다.
+    
+    :param ticker_symbol: str
+        다운로드할 티커
+    :param start_date: str
+        다운로드 시작 날짜 (YYYY-MM-DD 형식)
+    :param end_date: str
+        다운로드 종료 날짜 (YYYY-MM-DD 형식)
+    :return: DataFrame
+        다운로드된 데이터가 포함된 DataFrame
+    """
+    # yfinance의 download 메서드 사용
+    data = yf.download(
+        ticker_symbol,
+        start=start_date,
+        end=end_date,
+        interval='1m'  # 1분 간격
+    )
+    
+    return data
+
 if __name__ == '__main__' :
 
     symbol = f'095570.KS'
@@ -219,8 +81,12 @@ if __name__ == '__main__' :
 
     # 사용 예시
     ticker_symbol = '005930.KS'  # 삼성전자 티커
-    start_date = '2024-11-16'  # 시작 날짜
-    end_date = '2024-11-21'  # 종료 날짜
+    ticker_symbol = ["069500.KS", "252670.KS", "055550.KS", "010640.KS"] # 삼성전자 티커
+    start_date = '2024-11-1'  # 시작 날짜 # 모든 종목 최초 시점 찾고 end 에서 부터 -2d 정도 빼게
+    end_date = '2024-11-5'  # 종료 날짜 # datetime now로 가기 
+
+    start_date = datetime.now() - timedelta(days=5)  # 2일 전
+    end_date = datetime.now() + timedelta(days=1)    # 1일 후
 
     minute_data = download_minute_data(ticker_symbol, start_date, end_date)
     print(minute_data)
@@ -230,3 +96,16 @@ if __name__ == '__main__' :
     # 11.20 10시 5분 기준 11.18일 9시 부터 데이터 나옴 2일치가 최대 인듯
     # 인터벌로 돌아도 계속 밀릴듯
     # 이 버전은 좀 더 생각 해보기
+    '''
+    지금 까지 정리
+    1. 사실 8일 씩 뒤로 돌리면 되는 거였음. 언제 까지 되돌아갈 수 있는지 확인 필요 => last 30 days.
+    아마 이전에 테스트떄 장 마감일을 넣어서 테스트 한 거랑 invalid ticker로 테스트 해서 생긴 문제 였을 듯
+    2. invalid ticker 있으면 데이터 출력이 잘 안되나? 이거 자세히 다시 봐야 할 지도? 
+    개별로 하면 잘 되다가 묶어서 요청하면 잘 안나오는 경우가 있는 거 같음. 
+
+    symbols = ["069500.KS", "252670.KS", "055550.KS", "010640.KS"]
+    symbols = ["NVDA", "AAPL", "MSFT", "INVALID_SYMBOL", "AMZN", "META", "AVGO", "GOOGL", "TSLA", "GOOG", "BRK.B", '005930.KS', "010950.ks"]
+    symbols = ["NVDA", "AAPL", "MSFT", "AMZN", "META", "AVGO", "GOOGL", "TSLA", "GOOG"]
+    symbols = ["GOOG", "GOOGL", "TSLA"]
+
+    '''
